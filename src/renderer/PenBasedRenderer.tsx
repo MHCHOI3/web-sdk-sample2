ef7b07ccf23ff79e1fa30c97627d2956f6232570
minihallo-neo <loitermin@neolab.net>
2022-04-18 16:24:58 +0900

    Feat: viewer size 받을 수 있게 input ui 추가

--- src/renderer/PenBasedRenderer.tsx
+++ src/renderer/PenBasedRenderer.tsx
@@ -18,7 +18,7 @@ const PenBasedRenderer = () => {
   const [canvasFb, setCanvasFb] = useState<any>();
   const [ctx, setCtx] = useState<any>();
   // canvas size
-  const [width, height] = [940, 510];
+  // const [width, height] = [450, 510];
   // 3.27.387 paper size
   const [yMax, yMin] = [121.608635, 3.4970238];
   const [xMax, xMin] = [94.6116, 3.4970238];
@@ -28,46 +28,77 @@ const PenBasedRenderer = () => {
     setCanvasFb(initCanvas());
   }, []);
 
+  // Initialize Canvas
+  const initCanvas = () => { 
+    const canvas = new fabric.Canvas('sampleCanvas')
+    setCtx(canvas.getContext())
+    return canvas
+  }
+
   useEffect(() => {
-    PenHelper.dotCallback = (mac, dot) => {
-      setCtx(canvasFb.getContext());
+    PenHelper.dotCallback = async (mac, dot) => {
       strokeProcess(dot);
     }
   });
 
-  // Initialize Canvas
-  const initCanvas = () => { 
-    const canvas = new fabric.Canvas('sampleCanvas');
-    
-    return canvas
-  }
-
   const strokeProcess = (dot) => {
     // Calculate dot ratio
-    const dx = (dot.x * width) / xLen;
-    const dy = (dot.y * height) / yLen;
+    const dx = (dot.x * canvasFb.getWidth()) / xLen;
+    const dy = (dot.y * canvasFb.getHeight()) / yLen;
 
     // Pen Down
-    if (dot.dotType === 0) {
-      ctx.beginPath();
-    } else if (dot.dotType === 1) { // Pen Move
-      if (dot.x > 1000 || dot.y > 1000) {
-        return
+    try {
+      if (dot.dotType === 0) {
+          ctx.beginPath();
+      } else if (dot.dotType === 1) { // Pen Move
+        if (dot.x > 1000 || dot.y > 1000) {
+          return
+        }
+        ctx.lineWidth = 2;
+        ctx.lineTo(dx, dy);
+        ctx.stroke();
+        ctx.closePath();
+        ctx.beginPath();
+        ctx.moveTo(dx, dy);
+      } else {  // Pen Up
+        ctx.closePath();
       }
-      ctx.lineWidth = 2;
-      ctx.lineTo(dx, dy);
-      ctx.stroke();
-      ctx.closePath();
-      ctx.beginPath();
-      ctx.moveTo(dx, dy);
-    } else {  // Pen Up
-      ctx.closePath();
+    } catch {
+      console.log('ctx : ' + ctx);
     }
+  }
+  
+  const setCanvasWidth = (width) => {
+    canvasFb.setWidth(width);
+  }
+
+  const setCanvasHeight = (height) => {
+    canvasFb.setHeight(height);
   }
 
   return (
     <div className={classes.mainBackground}>
-      <canvas id="sampleCanvas" width={width} height={height}></canvas>
+      <div>
+        <label>Width: </label>
+        <input
+          id="width-input"
+          type="number"
+          disabled={false}
+          onChange={(e) => setCanvasWidth(parseInt(e.target.value))}
+        />
+      </div>
+      <div>
+        <label>Height: </label>
+        <input
+          id="height-input"
+          type="number"
+          disabled={false}
+          onChange={(e) => setCanvasHeight(parseInt(e.target.value))}
+        />
+      </div>
+      <div>
+        <canvas id="sampleCanvas"></canvas>
+      </div>
     </div>
   );
 };
