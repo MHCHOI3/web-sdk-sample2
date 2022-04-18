a1dbdea93668af1ce1c825af9c17f15604b65bf8
mminhou <exit19093@gmail.com>
2022-04-18 12:22:29 +0900

    Feat: Show note image, Canvas scaling(note image에 맞춘), 해당 canvas에 그려질수 있도록 설정

--- src/renderer/PenBasedRenderer.tsx
+++ src/renderer/PenBasedRenderer.tsx
@@ -2,32 +2,80 @@ import { makeStyles } from '@material-ui/core';
 import React, { useEffect, useState } from 'react';
 import PenHelper from '../utils/PenHelper';
 import { fabric } from 'fabric';
+import api from '../server/NoteServer';
+import { PageInfo } from '../utils/type';
 
 const useStyle = makeStyles(() => ({
   mainBackground: {
-    width: '100%',
-    height: '100%',
     display: 'flex',
     flexDirection: 'column',
     textAlign: 'center'
-  },
+  }
 }));
 
 const PenBasedRenderer = () => {
   const classes = useStyle();
   const [canvasFb, setCanvasFb] = useState<any>();
   const [ctx, setCtx] = useState<any>();
-  // canvas size
-  const [width, height] = [940, 510];
-  // 3.27.387 paper size
-  const [yMax, yMin] = [121.608635, 3.4970238];
-  const [xMax, xMin] = [94.6116, 3.4970238];
-  const [yLen, xLen] = [yMax-yMin, xMax-xMin];
 
+  const [pageInfo, setPageInfo] = useState<PageInfo>();
+  const [noteImage, setNoteImage] = useState<string>();
+
+  const [noteWidth, setNoteWidth] = useState(0);
+  const [noteHeight, setNoteHeight] = useState(0);
+
+  const [ncodeWidth, setNcodeWidth] = useState(0);
+  const [ncodeHeight, setNcodeHeight] = useState(0); 
+
+  // canvas size
   useEffect(() => {
     setCanvasFb(initCanvas());
   }, []);
 
+  useEffect(() => {
+    if (pageInfo) {
+      // Ncode Info
+      const ncodeSize = api.extractMarginInfo(pageInfo);
+      let ncodeWidth, ncodeHeight;
+      if (ncodeSize) {
+        ncodeWidth = ncodeSize.Xmax - ncodeSize.Xmin;
+        ncodeHeight = ncodeSize.Ymax - ncodeSize.Ymin;
+      }
+      setNcodeWidth(ncodeWidth);
+      setNcodeHeight(ncodeHeight);
+
+      // Note Info
+      const imageSrc = api.getNoteImage(pageInfo);
+      const { width, height } = api.getNoteSize(pageInfo);
+      setNoteImage(imageSrc);
+      setNoteWidth(width);
+      setNoteHeight(height);
+      
+    }
+  }, [pageInfo]);
+
+  useEffect(() => {
+    if (noteImage) {
+      /**
+       * Canvas width 재설정
+       * CanvasFb.height : CanvasFb.width = noteHeight : noteWidth;
+       * CanvasFb.width = (CanvasFb.height * noteWidth) / noteHeight;
+       */
+      // Canvas width 재설
+      const refactorCanvasWidth = canvasFb.height * noteWidth / noteHeight;
+      canvasFb.setWidth(refactorCanvasWidth);
+
+      canvasFb.setBackgroundImage(noteImage, canvasFb.renderAll.bind(canvasFb), {
+        scaleX: canvasFb.width / noteWidth,
+        scaleY: canvasFb.height / noteHeight,
+     });
+    }
+  }, [canvasFb, noteImage]);
+
+  useEffect(() => {
+    console.log(noteWidth, noteHeight);
+  }, [noteWidth, noteHeight]);
+ 
   useEffect(() => {
     PenHelper.dotCallback = (mac, dot) => {
       setCtx(canvasFb.getContext());
@@ -38,14 +86,26 @@ const PenBasedRenderer = () => {
   // Initialize Canvas
   const initCanvas = () => { 
     const canvas = new fabric.Canvas('sampleCanvas');
-    
+  
     return canvas
   }
 
   const strokeProcess = (dot) => {
+    /**
+     * Calculate dot ratio // 
+     * ncodeSize : ncodeDotPosition = canvasSize : canvasDotPosition
+     * canvasDotPosition = (ncodeDotPosition * canvasSize) / ncodeSize
+     * dot: ncodeDotPosition
+     * dx/dy: canvasDotPosition
+     * 
+     */
     // Calculate dot ratio
-    const dx = (dot.x * width) / xLen;
-    const dy = (dot.y * height) / yLen;
+    if (!pageInfo) {
+      setPageInfo(dot.pageInfo);
+    }
+
+    const dx = (dot.x * canvasFb.width) / ncodeWidth;
+    const dy = (dot.y * canvasFb.height) / ncodeHeight;
 
     // Pen Down
     if (dot.dotType === 0) {
@@ -67,7 +127,7 @@ const PenBasedRenderer = () => {
 
   return (
     <div className={classes.mainBackground}>
-      <canvas id="sampleCanvas" width={width} height={height}></canvas>
+      <canvas id="sampleCanvas" width={window.innerWidth} height={window.innerHeight-81}></canvas>
     </div>
   );
 };
