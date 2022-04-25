b95bdb2c77bbdea9984460863ac73af2a1b4ef6c
mminhou <exit19093@gmail.com>
2022-04-25 16:10:30 +0900

    Feat: plate pageInfo constant 정의

---
+++ src/utils/constants.ts
@@ -0,0 +1,20 @@
+export const PlateNcode_1 = {
+  section: 3,
+  owner: 1013,
+  book: 2,
+  page: 1,
+}
+
+export const PlateNcode_2 = {
+  section: 3,
+  owner: 1013,
+  book: 2,
+  page: 2,
+}
+
+export const PlateNcode_3 = { // 부기보드 === Smart Plate
+  section: 3,
+  owner: 1013,
+  book: 2,
+  page: 16,
+}
