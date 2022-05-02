944c2a00edc4d1ae99db399c0816002ea6d49faa
mminhou <exit19093@gmail.com>
2022-05-02 16:08:45 +0900

    Feat: web-sdk-sample project v2 업데이트

--- src/renderer/PenBasedRenderer.tsx
+++ src/renderer/PenBasedRenderer.tsx
@@ -1,4 +1,4 @@
-import { makeStyles, TextField } from '@material-ui/core';
+import { Button, makeStyles, TextField } from '@material-ui/core';
 import React, { useEffect, useState } from 'react';
 import { PenHelper, NoteServer } from 'web_pen_sdk';
 
@@ -13,21 +13,25 @@ const useStyle = makeStyles(() => ({
     textAlign: 'center',
     alignItems: 'center',
     position: 'relative',
+    height: window.innerHeight-163.25,
+    justifyContent: 'center',
+    backgroundColor: '#f5f5f5',
   },
   hoverCanvasContainer: {
     position: 'absolute',
   },
   mainCanvas: {
     position: 'absolute',
+    boxShadow: '1px 2px 6px rgba(0, 0, 0, 0.2)',
   },
   hoverCanvas: {
     position: 'absolute',
   },
   inputContainer: {
-    position: 'absolute',
+    display: 'flex',
+    justifyContent: 'center',
   },
   inputStyle: {
-    display: 'inline-block', 
     margin: 20,
   },
 }));
@@ -51,8 +55,10 @@ const PenBasedRenderer = () => {
 
   const [paperSize, setPaperSize] = useState<PaperSize>();
 
+  const [plateMode, setPlateMode] = useState<boolean>(false);
+
   useEffect(() => {
-    const { canvas, hoverCanvas } = initCanvas();
+    const { canvas, hoverCanvas } = createCanvas();
     setCanvasFb(canvas);
     setHoverCanvasFb(hoverCanvas);
   }, []);
@@ -60,9 +66,18 @@ const PenBasedRenderer = () => {
   // Note Image와 PaperSize를 설정하기 위한 부분
   useEffect(() => {
     async function getNoteImageUsingAPI(pageInfo) {
+      // pageInfo.section 이 0으로 들어오는 경우는 이상 pageInfo로 예외처리
+      if (pageInfo.section === 0) {
+        return;
+      } 
       await NoteServer.getNoteImage(pageInfo, setImageBlobUrl);
       const paperSize: any = await NoteServer.extractMarginInfo(pageInfo);
       setPaperSize(paperSize);
+      
+      // SmartPlate 일때는, 서버에서 가져온 이미지를 사용하지 않으므로 0으로 설정해준다. 
+      if (PenHelper.isSamePage(pageInfo, PlateNcode_3)) {
+        setImageBlobUrl(0);
+      }
     }
 
     if (pageInfo) {
@@ -89,9 +104,29 @@ const PenBasedRenderer = () => {
 
   useEffect(() => {
     if (noteWidth > 0 && noteHeight > 0) {
+      // pageInfo.section 이 0으로 들어오는 경우는 이상 pageInfo로 예외처리
+      if (pageInfo && pageInfo.section === 0) {
+        return 
+      }
+
+      // SmartPlate일 때에는 backgroundColor만 설정해 준 후 종료시켜준다.
+      if (pageInfo && PenHelper.isSamePage(pageInfo, PlateNcode_3)) {
+        canvasFb.backgroundColor = 'white';
+        canvasFb.renderAll();
+        return
+      }
+
+      // imageBlobUrl === 0 일때는 Canvas 재설정하는 작업이 불필요하다. 따라서 종료시켜준다.
+      if (imageBlobUrl === 0) {
+        return
+      }
+      
+      // Canvas를 기본값으로 초기화
+      setCanvasInitialize();
+
       /**
        * Canvas width 를 note image 비율에 맞춰 재설정 하는 로직
-       * 추가, Canvas height 는 기본적으로 'window.innerHeight - 81(Header의 높이)'로 되어있음.
+       * 추가, Canvas height 는 기본적으로 'window.innerHeight - 81(Header의 높이) - 82.25(input container의 높이)'로 되어있음.
        * 
        * noteWidth: note의 가로길이
        * noteHeight: note의 세로길이
@@ -103,7 +138,7 @@ const PenBasedRenderer = () => {
       const refactorCanvasWidth = canvasFb.height * noteWidth / noteHeight;
       canvasFb.setWidth(refactorCanvasWidth);
       hoverCanvasFb.setWidth(refactorCanvasWidth);
-      
+
       // NoteImage를 canvas 크기에 맞춰 보여지게 하기위한 scaling 작업
       canvasFb.setBackgroundImage(imageBlobUrl, canvasFb.renderAll.bind(canvasFb), {
         // noteImage를 Canvas의 크기에 맞게 resizing
@@ -115,7 +150,7 @@ const PenBasedRenderer = () => {
         left: [90, 180].includes(angle) ? canvasFb.width : 0,
       });      
     }
-  }, [noteWidth, noteHeight, angle]);
+  }, [noteWidth, noteHeight, angle, pageInfo]);
  
   // Ncode Dot을 받아오기 위한 dotCallback 설정
   useEffect(() => {
@@ -124,8 +159,8 @@ const PenBasedRenderer = () => {
     }
   });
 
-  // Initialize Canvas, hoverCanvas
-  const initCanvas = () => { 
+  // Create Canvas, hoverCanvas
+  const createCanvas = () => { 
     const canvas = new fabric.Canvas('mainCanvas');
     const hoverCanvas = new fabric.Canvas('hoverCanvas');
 
@@ -139,11 +174,20 @@ const PenBasedRenderer = () => {
    * @param dot { Dot }
    */
   const strokeProcess = (dot: Dot) => {
+    // SmartPlate를 사용하기 위해서는 Canvas의 width, height를 설정해야함.
+    if (PenHelper.isSamePage(dot.pageInfo, PlateNcode_3) && !plateMode) {
+      if (dot.dotType === 0) {  // PenDown을 한 경우에만 aler가 뜨도록 함.
+        alert('Plate Mode를 on으로 설정한 후, 캔버스를 생성해주세요.');
+      }
+      return
+    }
+
     /**
      * pageInfo란, Ncode 노트의 정보를 { section, owner, book, page }로 나타내는 값이며 어떤 Note를 사용하는지 파악하기 위해 사용한다
      * pageInfo 값이 존재하고, NULL_PageInfo({-1, -1, -1, -1})이 아닐 경우에만 pageInfo 상태를 갱신시켜준다.
      */ 
-     if (!pageInfo && !PenHelper.isSamePage(dot.pageInfo, NULL_PageInfo)) {
+    if ((!pageInfo && !PenHelper.isSamePage(dot.pageInfo, NULL_PageInfo)) || 
+        (pageInfo && !PenHelper.isSamePage(pageInfo, dot.pageInfo))) {
       setPageInfo(dot.pageInfo);
     }
 
@@ -169,10 +213,10 @@ const PenBasedRenderer = () => {
         ctx.beginPath();
         // PenDown 일때는 hoverPoint가 보일 필요가 없으므로 opacity 0으로 설정
         hoverPoint.set({ opacity: 0 });
-        hoverCanvasFb.requestRenderAll();      
+        hoverCanvasFb.requestRenderAll();
       } else if (dot.dotType === 1) { // Pen Move
         // dot 좌표가 너무 큰 값이 들어와버리면 이상 dot으로 취급하여 처리하지 않음.
-        if (dot.x > 1000 || dot.y > 1000) { 
+        if (dot.x > 800 || dot.y > 800) { 
           return
         }
         ctx.lineWidth = 2;
@@ -209,6 +253,35 @@ const PenBasedRenderer = () => {
   }
 
   /**
+   * Canvas의 가로길이를 설정하는 로직, SmartPlate 사용시 setting 해야함
+   * @param width { number }
+   */
+  const setCanvasWidth = (width: number) => {
+    canvasFb.setWidth(width);
+    hoverCanvasFb.setWidth(width);
+    canvasFb.setBackgroundImage(0, canvasFb.renderAll.bind(canvasFb))
+  }
+
+  /**
+   * Canvas의 세로길이를 설정하는 로직, SmartPlate 사용시 setting 해야함
+   * @param height { number }
+   */
+  const setCanvasHeight = (height: number) => {
+    canvasFb.setHeight(height);
+    hoverCanvasFb.setHeight(height);
+    canvasFb.setBackgroundImage(0, canvasFb.renderAll.bind(canvasFb))
+  }
+
+  // Canvas를 width, height, angle, plateMode를 초기화 시키기 위한 로직 
+  const setCanvasInitialize = () => {
+    setPlateMode(false);
+    // header높이(81) + 아래 inputContainer높이(82.25) + margin 셋팅을 위한 값 셋팅(20)
+    canvasFb.setHeight(window.innerHeight-163.25-20);
+    hoverCanvasFb.setHeight(window.innerHeight-163.25-20);
+    setAngle(0);
+  }
+
+  /**
    * Hover Point를 이동시키기 위한 로직
    * @param screenDot { ScreenDot }
    */
@@ -232,18 +305,41 @@ const PenBasedRenderer = () => {
   }
 
   return (
-    <div className={classes.mainBackground}>
-      <canvas id="mainCanvas" className={classes.mainCanvas} width={window.innerWidth} height={window.innerHeight-81}></canvas>
-      <div className={classes.hoverCanvasContainer}>
-        <canvas id="hoverCanvas" className={classes.hoverCanvas} width={window.innerWidth} height={window.innerHeight-81}></canvas>
+    <>
+      <div className={classes.mainBackground}>
+        <canvas id="mainCanvas" className={classes.mainCanvas} width={window.innerWidth} height={window.innerHeight-163.25}></canvas>
+        <div className={classes.hoverCanvasContainer}>
+          <canvas id="hoverCanvas" className={classes.hoverCanvas} width={window.innerWidth} height={window.innerHeight-163.25}></canvas>
+        </div>
       </div>
       <div className={classes.inputContainer}>
         <div className={classes.inputStyle}>
-          <TextField id="angle" label="Angle" variant="outlined" type="number" size="small"
-              onChange={(e) => setCanvasAngle(parseInt(e.target.value))} />
+          <Button variant="contained" color="primary" size="large" onClick={() => setPlateMode(!plateMode)}>
+            { plateMode ? 'Plate mode off' : 'Plate mode on' }
+          </Button>
         </div>
+        { plateMode ? 
+          <div className={classes.inputContainer}>
+            <div className={classes.inputStyle}>
+              <TextField id="width" label="Width" variant="outlined" type="number" size="small"
+                  onChange={(e) => setCanvasWidth(parseInt(e.target.value))} 
+                  disabled={!plateMode}
+                  />
+            </div>
+            <div className={classes.inputStyle}>
+              <TextField id="height" label="Height" variant="outlined" type="number" size="small"
+                  onChange={(e) => setCanvasHeight(parseInt(e.target.value))} 
+                  disabled={!plateMode}
+                  />
+            </div>
+            <div className={classes.inputStyle}>
+              <TextField id="angle" label="Angle" variant="outlined" type="number" size="small"
+                  onChange={(e) => setCanvasAngle(parseInt(e.target.value))} />
+            </div>
+          </div> : ''
+        }
       </div>
-    </div>
+    </>
   );
 };
 
