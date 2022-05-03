e711b117c3a6355644be07ee8bdeb6645b18091b
mminhou <exit19093@gmail.com>
2022-05-03 11:08:53 +0900

    Feat: 전체적인 주석 수정 및 불필요한 코드 정리작업

--- src/renderer/PenBasedRenderer.tsx
+++ src/renderer/PenBasedRenderer.tsx
@@ -63,20 +63,21 @@ const PenBasedRenderer = () => {
     setHoverCanvasFb(hoverCanvas);
   }, []);
 
-  // Note Image와 PaperSize를 설정하기 위한 부분
+  /** Setting Ncode noteImage/paperSize */ 
   useEffect(() => {
     async function getNoteImageUsingAPI(pageInfo) {
-      // pageInfo.section 이 0으로 들어오는 경우는 이상 pageInfo로 예외처리
-      if (pageInfo.section === 0) {
+      if (pageInfo.section === 0) {  // pageInfo.section === 0 -> abnormal pageInfo
         return;
       } 
       await NoteServer.getNoteImage(pageInfo, setImageBlobUrl);
       const paperSize: any = await NoteServer.extractMarginInfo(pageInfo);
       setPaperSize(paperSize);
       
-      // SmartPlate 일때는, 서버에서 가져온 이미지를 사용하지 않으므로 0으로 설정해준다. 
       if (PenHelper.isSamePage(pageInfo, PlateNcode_3)) {
+        // SmartPlate Case, 서버에서 가져온 이미지를 사용하지 않으므로 0으로 설정해주고, canvasFb의 backgroundColor를 white로 만들어준다.
         setImageBlobUrl(0);
+        canvasFb.backgroundColor = 'white';
+        canvasFb.renderAll();
       }
     }
 
@@ -91,6 +92,7 @@ const PenBasedRenderer = () => {
     }
   }, [hoverCanvasFb]);
 
+  /** Get noteImage size */
   useEffect(() => {
     if (imageBlobUrl) {
       const image = new Image();
@@ -104,47 +106,35 @@ const PenBasedRenderer = () => {
 
   useEffect(() => {
     if (noteWidth > 0 && noteHeight > 0) {
-      // pageInfo.section 이 0으로 들어오는 경우는 이상 pageInfo로 예외처리
-      if (pageInfo && pageInfo.section === 0) {
+      if (pageInfo && pageInfo.section === 0) {  // pageInfo.section === 0 -> abnormal pageInfo
         return 
       }
 
-      // SmartPlate일 때에는 backgroundColor만 설정해 준 후 종료시켜준다.
-      if (pageInfo && PenHelper.isSamePage(pageInfo, PlateNcode_3)) {
-        canvasFb.backgroundColor = 'white';
-        canvasFb.renderAll();
+      if (pageInfo && PenHelper.isSamePage(pageInfo, PlateNcode_3)) {  // In case of SmartPlate, not required bottom process.
         return
       }
 
-      // imageBlobUrl === 0 일때는 Canvas 재설정하는 작업이 불필요하다. 따라서 종료시켜준다.
-      if (imageBlobUrl === 0) {
+      if (imageBlobUrl === 0) {  // In case of 'imageBlobUrl === 0', not required bottom process.
         return
       }
       
-      // Canvas를 기본값으로 초기화
       setCanvasInitialize();
 
       /**
-       * Canvas width 를 note image 비율에 맞춰 재설정 하는 로직
-       * 추가, Canvas height 는 기본적으로 'window.innerHeight - 81(Header의 높이) - 82.25(input container의 높이)'로 되어있음.
+       * Refactoring canvas width based on noteImage.
        * 
-       * noteWidth: note의 가로길이
-       * noteHeight: note의 세로길이
-       * 
+       * Canvas(View) default height = 'window.innerHeight - 81(Header) - 82.25(input container)'
        * CanvasFb.height : CanvasFb.width = noteHeight : noteWidth;
        * CanvasFb.width(=refactorCanvasWidth) = (CanvasFb.height * noteWidth) / noteHeight;
-       * 
        */
       const refactorCanvasWidth = canvasFb.height * noteWidth / noteHeight;
       canvasFb.setWidth(refactorCanvasWidth);
       hoverCanvasFb.setWidth(refactorCanvasWidth);
 
-      // NoteImage를 canvas 크기에 맞춰 보여지게 하기위한 scaling 작업
-      canvasFb.setBackgroundImage(imageBlobUrl, canvasFb.renderAll.bind(canvasFb), {
-        // noteImage를 Canvas의 크기에 맞게 resizing
+      canvasFb.setBackgroundImage(imageBlobUrl, canvasFb.renderAll.bind(canvasFb), {  // Resizing noteImage to fit canvas size
         scaleX: canvasFb.width / noteWidth,
         scaleY: canvasFb.height / noteHeight,
-        // backgroundImage angle 설정에 따른 top, left 값 수정
+        // backgroundImage angle setting
         angle: angle,
         top: [180, 270].includes(angle) ? canvasFb.height : 0,
         left: [90, 180].includes(angle) ? canvasFb.width : 0,
@@ -152,14 +142,18 @@ const PenBasedRenderer = () => {
     }
   }, [noteWidth, noteHeight, angle, pageInfo]);
  
-  // Ncode Dot을 받아오기 위한 dotCallback 설정
+  /**
+   * This callback type is called `dotCallback`.
+   * 
+   * @callback dotCallback
+   */
   useEffect(() => {
     PenHelper.dotCallback = async (mac, dot) => {
       strokeProcess(dot);
     }
   });
 
-  // Create Canvas, hoverCanvas
+  /** Create mainCanvas, hoverCanvas */
   const createCanvas = () => { 
     const canvas = new fabric.Canvas('mainCanvas');
     const hoverCanvas = new fabric.Canvas('hoverCanvas');
@@ -170,22 +164,19 @@ const PenBasedRenderer = () => {
   }
 
   /**
-   * Ncode dot을 사용하기 위해 처리하는 로직
-   * @param dot { Dot }
+   * Process ncode dot.
+   * 
+   * @param {Dot} dot
    */
   const strokeProcess = (dot: Dot) => {
-    // SmartPlate를 사용하기 위해서는 Canvas의 width, height를 설정해야함.
-    if (PenHelper.isSamePage(dot.pageInfo, PlateNcode_3) && !plateMode) {
-      if (dot.dotType === 0) {  // PenDown을 한 경우에만 aler가 뜨도록 함.
+    if (PenHelper.isSamePage(dot.pageInfo, PlateNcode_3) && !plateMode) {  // SmartPlate를 터치했는데 plateMode가 on으로 설정되지 않으면 사용하지 못하도록 함.
+      if (dot.dotType === 0) {  // Show alert message only if penDown
         alert('Plate Mode를 on으로 설정한 후, 캔버스를 생성해주세요.');
       }
       return
     }
 
-    /**
-     * pageInfo란, Ncode 노트의 정보를 { section, owner, book, page }로 나타내는 값이며 어떤 Note를 사용하는지 파악하기 위해 사용한다
-     * pageInfo 값이 존재하고, NULL_PageInfo({-1, -1, -1, -1})이 아닐 경우에만 pageInfo 상태를 갱신시켜준다.
-     */ 
+    /** Update pageInfo either pageInfo !== NULL_PageInfo or pageInfo changed */ 
     if ((!pageInfo && !PenHelper.isSamePage(dot.pageInfo, NULL_PageInfo)) || 
         (pageInfo && !PenHelper.isSamePage(pageInfo, dot.pageInfo))) {
       setPageInfo(dot.pageInfo);
@@ -199,24 +190,22 @@ const PenBasedRenderer = () => {
       return;
     }
 
-    // Ncode Dot을 view(Canvas) size 에 맞춰 좌표값을 변환시켜준다.
+    /** Convert SmartPlate ncode dot coordinate values ​​according to the view size */
     const view = { width: canvasFb.width, height: canvasFb.height };
     let screenDot: ScreenDot;
-    if (PenHelper.isSamePage(dot.pageInfo, PlateNcode_3)) { // Smart Plate
+    if (PenHelper.isSamePage(dot.pageInfo, PlateNcode_3)) {  // Smart Plate
       screenDot = PenHelper.ncodeToScreen_smartPlate(dot, view, angle, paperSize);
     } else {  // Default
       screenDot = PenHelper.ncodeToScreen(dot, view, paperSize);
     }
 
     try {
-      if (dot.dotType === 0) { // Pen Down
+      if (dot.dotType === 0) {  // Pen Down
         ctx.beginPath();
-        // PenDown 일때는 hoverPoint가 보일 필요가 없으므로 opacity 0으로 설정
-        hoverPoint.set({ opacity: 0 });
+        hoverPoint.set({ opacity: 0 });  // In case of PenDown, hoverPoint dont need to look
         hoverCanvasFb.requestRenderAll();
-      } else if (dot.dotType === 1) { // Pen Move
-        // dot 좌표가 너무 큰 값이 들어와버리면 이상 dot으로 취급하여 처리하지 않음.
-        if (dot.x > 800 || dot.y > 800) { 
+      } else if (dot.dotType === 1) {  // Pen Move
+        if (dot.x > 800 || dot.y > 800) {  // Abnormal dot point -> return
           return
         }
         ctx.lineWidth = 2;
@@ -225,9 +214,9 @@ const PenBasedRenderer = () => {
         ctx.closePath();
         ctx.beginPath();
         ctx.moveTo(screenDot.x, screenDot.y);
-      } else if (dot.dotType === 2) { // Pen Up
+      } else if (dot.dotType === 2) {  // Pen Up
         ctx.closePath();
-      } else if (dot.dotType === 3) { // Hover 
+      } else if (dot.dotType === 3) {  // Hover 
         hoverProcess(screenDot);
       }
     } catch {
@@ -236,15 +225,15 @@ const PenBasedRenderer = () => {
   }
 
   /**
-   * Canvas의 각도를 설정하는 로직, SmartPlate에서만 동작하도록 완성
-   * @param rotate { number }
+   * Set canvas angle.
+   * 
+   * @param {number} rotate
    */
   const setCanvasAngle = (rotate: number) => {
     if (![0, 90, 180, 270].includes(rotate)) return
     if (!pageInfo || !PenHelper.isSamePage(pageInfo, PlateNcode_3)) return
 
-    // 90', 270' 일 경우 note의 width <-> height를 swap 시켜준다. 
-    if (Math.abs(angle-rotate)/90 === 1 || Math.abs(angle-rotate)/90 === 3) {
+    if (Math.abs(angle-rotate)/90 === 1 || Math.abs(angle-rotate)/90 === 3) {  // 90', 270' - swap noteWidth <-> noteHeight
       const tmp = noteWidth;
       setNoteWidth(noteHeight);
       setNoteHeight(tmp); 
@@ -253,8 +242,9 @@ const PenBasedRenderer = () => {
   }
 
   /**
-   * Canvas의 가로길이를 설정하는 로직, SmartPlate 사용시 setting 해야함
-   * @param width { number }
+   * Set canvas width.
+   * 
+   * @param {number} width
    */
   const setCanvasWidth = (width: number) => {
     canvasFb.setWidth(width);
@@ -263,8 +253,9 @@ const PenBasedRenderer = () => {
   }
 
   /**
-   * Canvas의 세로길이를 설정하는 로직, SmartPlate 사용시 setting 해야함
-   * @param height { number }
+   * Set canvas height.
+   * 
+   * @param {number} height
    */
   const setCanvasHeight = (height: number) => {
     canvasFb.setHeight(height);
@@ -272,18 +263,18 @@ const PenBasedRenderer = () => {
     canvasFb.setBackgroundImage(0, canvasFb.renderAll.bind(canvasFb))
   }
 
-  // Canvas를 width, height, angle, plateMode를 초기화 시키기 위한 로직 
+  /** Initialize Canvas width, height, angle and plateMode */
   const setCanvasInitialize = () => {
     setPlateMode(false);
-    // header높이(81) + 아래 inputContainer높이(82.25) + margin 셋팅을 위한 값 셋팅(20)
-    canvasFb.setHeight(window.innerHeight-163.25-20);
+    canvasFb.setHeight(window.innerHeight-163.25-20);  // header(81) + inputContainer(82.25) + margin value(20)
     hoverCanvasFb.setHeight(window.innerHeight-163.25-20);
     setAngle(0);
   }
 
   /**
-   * Hover Point를 이동시키기 위한 로직
-   * @param screenDot { ScreenDot }
+   * Hover Point Process.
+   * 
+   * @param {ScreenDot} screenDot
    */
   const hoverProcess = (screenDot: ScreenDot) => {
     hoverPoint.set({ left: screenDot.x, top: screenDot.y, opacity: 0.5 });
@@ -323,13 +314,11 @@ const PenBasedRenderer = () => {
             <div className={classes.inputStyle}>
               <TextField id="width" label="Width" variant="outlined" type="number" size="small"
                   onChange={(e) => setCanvasWidth(parseInt(e.target.value))} 
-                  disabled={!plateMode}
                   />
             </div>
             <div className={classes.inputStyle}>
               <TextField id="height" label="Height" variant="outlined" type="number" size="small"
                   onChange={(e) => setCanvasHeight(parseInt(e.target.value))} 
-                  disabled={!plateMode}
                   />
             </div>
             <div className={classes.inputStyle}>
